cd8d2a92922e46a71ae5cf18057242efff393b36
Ricardo Albuquerque <rchard.asc@gmail.com>
2026-04-29 19:24:15 -0300

    update test_checkout.py

--- saucedemo-selenium/tests/test_checkout.py
+++ saucedemo-selenium/tests/test_checkout.py
@@ -43,20 +43,22 @@ def test_fluxo_completo_de_compra():
         ).text
 
         assert produto == "Sauce Labs Backpack"
-
+        
         # 7. Ir para checkout
         wait.until(
             EC.element_to_be_clickable((By.ID, "checkout"))
         ).click()
-
+        
+        # Esperar a página de checkout carregar
+        wait.until(EC.url_contains("checkout-step-one"))
+        
         # 8. Preencher dados do cliente
-        wait.until(EC.visibility_of_element_located((By.ID, "first-name"))).send_keys("Ricardo")
+        wait.until(
+            EC.visibility_of_element_located((By.ID, "first-name"))
+        ).send_keys("Ricardo")
+        
         driver.find_element(By.ID, "last-name").send_keys("Albuquerque")
         driver.find_element(By.ID, "postal-code").send_keys("64000-000")
-
-        wait.until(
-            EC.element_to_be_clickable((By.ID, "continue"))
-        ).click()
 
         # 9. Validar resumo da compra
         wait.until(EC.url_contains("checkout-step-two"))
